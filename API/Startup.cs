69d2c9f7bae24f70767aeff180a24b7d8a9b9459
Mikhail <mikezhe@yahoo.com>
2021-10-28 22:06:44 -0500

    end of 4

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,7 +13,11 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
-
+using Application.Activities;
+using MediatR;
+using AutoMapper;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -33,23 +37,8 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-           {
-               opt.UseSqlite(_config.GetConnectionString("Defaultconnection"));
-
-           }
-
-            );
-            services.AddCors(opt =>
-            {
-            opt.AddPolicy("CorsPolicy",policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-            });
-        });
+            services.AddApplicationServices(_config);
+         ;
         }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
---
+++ Application/Activities/List.cs
@@ -0,0 +1,48 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+      
+            public Handler(DataContext context )
+            {
+            
+                _context = context;
+            }
+
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+               /*  try{
+for (int i=0; i<10;i++)
+{
+    cancellationToken.ThrowIfCancellationRequested();
+    await Task.Delay(1000,cancellationToken);
+    _logger.LogInformation($"Task {i} completed");
+
+}
+                }
+
+                catch(Exception ex)  when (ex is TaskSchedulerException)
+                {
+_logger.LogInformation($"Task was cancelled");
+                } */
+               return  await _context.Activities.ToListAsync(cancellationToken);
+            }
+        }
+    }
+}
