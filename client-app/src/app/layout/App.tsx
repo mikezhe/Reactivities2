502686f834b368315961d2c871d4fa1df1ccbd6b
mikezhe <mikezhe@yahoo.com>
2021-10-31 19:44:18 -0500

    aded more

--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,23 +3,15 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import { Header , List} from 'semantic-ui-react';
+import {Activity} from '../models/activity';
 
 
-interface ServerData {
-  Id : string
-  Title: string,
-  Description: string,
-  Date: Date,
-   Category  :string ,
-   City : string,
- Venue: string
-}
 function App() {
 
 
-const [activities,setActivities] = useState<ServerData[]>();
+const [activities,setActivities] = useState<Activity[]>([]);
 useEffect(() => {
-  axios.get<ServerData[]>('http://localhost:5000/api/activities').then(response => {
+  axios.get<Activity[]>('http://localhost:5000/api/activities').then(response => {
 
    
     setActivities(response.data);
@@ -30,15 +22,14 @@ useEffect(() => {
     <div>
       <Header as ='h2' icon='users' content='Reactivities'/>
 
-  
+
       <List>
-      {activities?.map(function (activity: any) {
-            return (
+      {activities.map(activity => (
               <List.Item key={activity.id}>
                 {activity.title}
               </List.Item>
-            );
-          })}
+            )
+          )}
       </List>
         
      
